c9d5b64fb70291214a527d70f9c1672340b8c6b7
niklas-x <silver.nz2004@gmail.com>
2024-12-05 16:33:32 +0100

    Update Motor2.java

--- Fertige Klassen/Motor/Motor2.java
+++ Fertige Klassen/Motor/Motor2.java
@@ -5,26 +5,27 @@ import ch.aplu.ev3.*;
 public class Motor2                         
 {     
   int strecke = 3000;
+  int grad90  = 700;
   
   public Motor2()                         
   {                               
     LegoRobot robot = new LegoRobot();              
     
-      Gear gear = new Gear();                   
-      robot.addPart(gear);                    
+    Gear gear = new Gear();                   
+    robot.addPart(gear);                    
 
     //while (!robot.isEscapeHit())                
     //{                             
       for (int i = 0; i < 3; i++)               
       {                           
         gear.forward(strecke);                  
-        gear.right(700);                  
+        gear.right(grad90);                  
         gear.forward(strecke);                   
-        gear.right(700);                  
+        gear.right(grad90);                  
         gear.forward(strecke);                  
-        gear.left(700);                   
+        gear.left(grad90);                   
         gear.forward(strecke);                  
-        gear.left(700);                   
+        gear.left(grad90);                   
       }                           
       gear.forward(strecke);  
       
