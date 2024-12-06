5c3fd33158f8cf55b187268f13daaab6b34599b6
niklas-x <silver.nz2004@gmail.com>
2024-12-06 01:11:25 +0100

    Update UltraschallsensorX1.java

--- test2/UltraschallsensorX1.java
+++ test2/UltraschallsensorX1.java
@@ -8,13 +8,13 @@ public class Ultraschallsensor1 						//die Klasse "Ultraschallsensor1" führt d
 	{
 	 LegoRobot robot = new LegoRobot();					//deklariert und initialisiert ein neues LegoRobot Objekt mit dem Namen "robot"		
 	
-	 Gear gear = new Gear();
-	 robot.addPart(gear);
+	 Gear gear = new Gear();						//erzeugt ein Fahrwerk namens "gear" mit Motoren an den Anschlüssen A und B 
+	 robot.addPart(gear);							//fügt dem Roboterobjekt "robot" das Fahtwerk "gear" zu
 	
-	 UltrasonicSensor us = new UltrasonicSensor(SensorPort.S1);
-    	 robot.addPart(us);
+	 UltrasonicSensor us = new UltrasonicSensor(SensorPort.S1);		//am Sensorport 1 ist ein Ultrasonic-Sensor angeschlossen namens "us"
+    	 robot.addPart(us);							//der Ultrasonic-Sensor (us) wird zum "robot" hinzugefügt
 	
-	 int counter = 0;
+	 int counter = 0;							//in den Integer "counter" wird der Wert 0 gespeichert
 		
 	
 	 gear.forward();
