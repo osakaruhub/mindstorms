bb0a602e2d5f5c43365e5f3a752d3a9dc14123ae
niklas-x <silver.nz2004@gmail.com>
2024-12-06 01:02:33 +0100

    Update UltraschallsensorX1.java

--- test2/UltraschallsensorX1.java
+++ test2/UltraschallsensorX1.java
@@ -1,82 +1,60 @@
-package test;
+package Ultraschallsensor;							//die Klassen "Ultraschallsensor1", "Motor2", "Motor3", "Motor4" liegen alle im Package "Motor"
 
 import ch.aplu.ev3.*;
 
-public class UltraschallsensorX1 
+public class Ultraschallsensor1 
 {
-	public UltraschallsensorX1()
+	public Ultraschallsensor1()
 	{
 
-	LegoRobot robot = new LegoRobot();
+	 LegoRobot robot = new LegoRobot();
 	
-	Gear gear = new Gear();
-	robot.addPart(gear);
+	 Gear gear = new Gear();
+	 robot.addPart(gear);
 	
-	UltrasonicSensor us = new UltrasonicSensor(SensorPort.S1);
-    robot.addPart(us);
+	 UltrasonicSensor us = new UltrasonicSensor(SensorPort.S1);
+    	 robot.addPart(us);
 	
-	int x = 0;
-	int counter = 0;
-	
-	gear.forward();
-	
-	
-	
-	 //while (!robot.isEscapeHit())	
-	 try {
-		 while(x <= 5)
-		 {
-			 
+	 int counter = 0;
 		
-			 int distance = us.getDistance();
-			 
-		if(distance <= 5 && counter % 2 == 0)
+	
+	 gear.forward();
+	
+	 //while (!robot.isEscapeHit())
+	 //{	
+		while(counter <= 5)
 		{
-		gear.backward(1000);
-		gear.right(700);
-		gear.forward(1000);
-		gear.right(700);
-		Thread.sleep(1000);
-		gear.forward(1000);
-		gear.forward();
-		counter++;
-		x++;
-		}
+			int distance = us.getDistance();
+			 
+			if(distance <= 5 && counter % 2 == 0)
+			{
+			 gear.backward(1000);
+			 gear.right(700);
+			 gear.forward(1000);
+			 gear.right(700);
+			 gear.forward(1000);
+			 gear.forward();
+			 counter++;
+			}
 		
-		if(distance <= 5 && counter % 2 == 1)
-		{
-		
-		gear.backward(1000);
-		gear.left(700);
-		gear.forward(1000);
-		gear.left(700);
-		Thread.sleep(1000);
-		gear.forward(1000);
-		gear.forward();
-		counter--;
-		x++;
-		}
-		
-			 }
-			
-		
+			if(distance <= 5 && counter % 2 == 1)
+			{
+			 gear.backward(1000);
+			 gear.left(700);
+			 gear.forward(1000);
+			 gear.left(700);
+			 gear.forward(1000);
+			 gear.forward();
+			 counter++;
+			}
+		}	
+	robot.exit();
+	    //}	
 	}
-	
-	 catch(Exception e)
-	 {
-		 
-	 }
-	 
-	 robot.exit();
-	
-	
-	}
-	
-	
 	
 	public static void main(String [] args)
 	{
-		new UltraschallsensorX1 ();
+		new Ultraschallsensor1();
 	}
 
 }
